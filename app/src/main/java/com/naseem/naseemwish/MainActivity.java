d77ec5820d23148fa5bac99db0a361a916887c4b
Naseem Badran <nbadran68@gmail.com>
2017-10-04 13:23:59 +0300

    yhn

--- app/src/main/java/com/naseem/naseemwish/MainActivity.java
+++ app/src/main/java/com/naseem/naseemwish/MainActivity.java
@@ -1,13 +1,44 @@
 package com.naseem.naseemwish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPass;
+    private Button btnSignin;
+    private Button btnSignup;
+    private Button btnForget;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSignin=(Button)findViewById(R.id.btnSignin);
+        btnForget=(Button)findViewById(R.id.btnForget);
+        btnSignup=(Button)findViewById(R.id.btnSignup);
+        etEmail=(EditText)findViewById(R.id.etEmail);
+        etPass=(EditText)findViewById(R.id.etPass);
+
+        public void Onlclick(View v)
+    {
+        if(v==btnSignup)
+        {
+            Intent intent=new Intent(this,SignUP.class);
+            startActivity(intent);
+
+        }
+
+
+    }
+
     }
 }
--- app/src/main/java/com/naseem/naseemwish/SignUP.java
+++ app/src/main/java/com/naseem/naseemwish/SignUP.java
@@ -2,12 +2,30 @@ package com.naseem.naseemwish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUP extends AppCompatActivity {
+
+    private EditText etName;
+    private EditText etEmail2;
+    private EditText etPass2;
+    private EditText etRepass;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etName=(EditText)findViewById(R.id.etName);
+        etEmail2=(EditText)findViewById(R.id.etEmail2);
+        etPass2=(EditText)findViewById(R.id.etPass2);
+        etRepass=(EditText)findViewById(R.id.etRepass);
+        btnSave=(Button)findViewById(R.id.btnSave);
+
+
+
     }
 }
