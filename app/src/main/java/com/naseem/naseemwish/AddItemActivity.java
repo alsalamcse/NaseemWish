a23779fd1a1880467b09883431c2558b86b5a559
Naseem Badran <nbadran68@gmail.com>
2017-10-12 14:57:22 +0300

    button

--- app/src/main/java/com/naseem/naseemwish/AddItemActivity.java
+++ app/src/main/java/com/naseem/naseemwish/AddItemActivity.java
@@ -1,7 +1,10 @@
 package com.naseem.naseemwish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
     }
+
+
 }
