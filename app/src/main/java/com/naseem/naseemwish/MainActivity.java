cfe1ee8451b1b4312fd861cfff11d89ff1bed642
Naseem Badran <nbadran68@gmail.com>
2017-10-15 11:39:16 +0300

    button

--- app/src/main/java/com/naseem/naseemwish/MainActivity.java
+++ app/src/main/java/com/naseem/naseemwish/MainActivity.java
@@ -1,12 +1,19 @@
 package com.naseem.naseemwish;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.naseem.naseemwish.mainlistfragments.MainListActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSignin;
     private Button btnSignup;
     private Button btnForget;
+
 
 
     @Override
@@ -30,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPass = (EditText) findViewById(R.id.etPass);
     }
+
+
+
 
     public void Onlclick(View v) {
         if (v == btnSignup) {
--- app/src/main/java/com/naseem/naseemwish/SignUP.java
+++ app/src/main/java/com/naseem/naseemwish/SignUP.java
@@ -1,11 +1,20 @@
 package com.naseem.naseemwish;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.naseem.naseemwish.mainlistfragments.MainListActivity;
 
 public class SignUP extends AppCompatActivity {
 
@@ -14,12 +23,24 @@ public class SignUP extends AppCompatActivity {
     private EditText etPass2;
     private EditText etRepass;
     private Button btnSave;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, MainListActivity.class));
+            finish();
+            ;
+            return;
+        } else {
+            String userName = firebaseUser.getDisplayName();
+        }
 
         etName = (EditText) findViewById(R.id.etName);
         etEmail2 = (EditText) findViewById(R.id.etEmail2);
@@ -27,16 +48,49 @@ public class SignUP extends AppCompatActivity {
         etRepass = (EditText) findViewById(R.id.etRepass);
         btnSave = (Button) findViewById(R.id.btnSave);
     }
-    public void onCliclk(View v)
-    {
-        if(btnSave==v)
+
+
+    private void dataHandler() {
+        String stEmail = etEmail2.getText().toString();
+        String stName = etName.getText().toString();
+        String stPassw = etPass2.getText().toString();
+        String stRepassw = etRepass.getText().toString();
+        boolean isok = true;
+        if(stEmail.length()==0 || stEmail.indexOf('@')<1)
         {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            etEmail2.setError("Wrong Email");
+            isok=false;
         }
+        if(stPassw.length()<8 || stPassw.equals(stRepassw)==false)
+        {
+            etPass2.setError("Bad Password");
+            isok=false;
+        }
+        craetAcount(stEmail, stPassw);
+    }
 
 
+    public void onClick(View v) {
+        dataHandler();
+    }
+
+    private void craetAcount(String email, String passw) {
+        auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(SignUP.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignUP.this, "Authentication Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(SignUP.this, "Authentication failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
 
 
+        });
     }
 }
+
+
+
