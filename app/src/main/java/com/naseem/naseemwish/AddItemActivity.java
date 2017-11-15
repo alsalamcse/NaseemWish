3f14b74fbfc2f5a101574698167143b4cbaa3650
Naseem Badran <nbadran68@gmail.com>
2017-11-15 10:51:10 +0200

    button

--- app/src/main/java/com/naseem/naseemwish/AddItemActivity.java
+++ app/src/main/java/com/naseem/naseemwish/AddItemActivity.java
@@ -5,14 +5,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    private EditText etName;
+    private EditText etAmount;
+    private EditText etUnits;
+    private EditText etPrice;
+    private ImageButton iBtnImage;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        etName=(EditText)findViewById(R.id.etName);
+        etAmount=(EditText)findViewById(R.id.etAmount);
+        etUnits=(EditText)findViewById(R.id.etUnits);
+        etPrice=(EditText)findViewById(R.id.etPrice);
+        iBtnImage=(ImageButton)findViewById(R.id.iBtnImage);
+        btnSave=(Button)findViewById(R.id.btnSave);
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datahandler();
+            }
+        });
+    }
+    public void datahandler()
+    {
+        String stName=etName.getText().toString();
+        String stUnits=etUnits.getText().toString();
+        String stAmount=etAmount.getText().toString();
+        String stPrice=etPrice.getText().toString();
+        double amount=Double.parseDouble(stAmount);
+        double price=Double.parseDouble(stPrice);
+
+        DatabaseReference reference;
     }
 
 
